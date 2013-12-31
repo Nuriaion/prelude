ef387185114cc23af019e7a53ffdbe8ca7277a75
Fabian <faebu3007@gmail.com>
2013-12-31 03:40:42 +0100

    wip

--- personal/personal.el
+++ personal/personal.el
@@ -33,5 +33,26 @@
 
 (smartparens-strict-mode)
 
+(erc-autojoin-mode t)
+(setq erc-autojoin-channels-alist
+      '((".*\\.freenode.net" "#emacs" "#overtone")))
+
+(erc-track-mode t)
+(setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"
+
+                                "324" "329" "332" "333" "353" "477"))
+;; don't show any of this
+(setq erc-hide-list '("JOIN" "PART" "QUIT" "NICK"))
+
+
+(require 'clj-refactor)
+(add-hook 'clojure-mode-hook (lambda ()
+                               (clj-refactor-mode 1)
+                               ;; insert keybinding setup here
+                               (cljr-add-keybindings-with-prefix "M-r")
+                               ))
+
+
+
 (provide 'personal)
 ;;; personal.el ends here
